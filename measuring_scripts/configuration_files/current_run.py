5d5e7afdfba87747124ddfcd406e69443f5bd6d9
Matias Senger <m.senger@hotmail.com>
2022-10-12 18:14:24 +0200

    Current configuration

--- measuring_scripts/configuration_files/current_run.py
+++ measuring_scripts/configuration_files/current_run.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
 
-Alberto = RunBureaucrat(Path.home()/Path('measurements_data/20220831000000_TwoRealDevices'))
+Alberto = RunBureaucrat(Path.home()/Path('measurements_data/20221006_IrradiatedTI_Load2'))
 Alberto.create_run()
