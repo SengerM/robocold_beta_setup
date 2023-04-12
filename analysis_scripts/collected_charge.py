7ab10812a609e5e9c61cf8a11b12d2a25297e192
Matias Senger <m.senger@hotmail.com>
2023-04-12 11:46:41 +0200

    Added `use_clean_beta_scan` which makes it more stable

--- analysis_scripts/collected_charge.py
+++ analysis_scripts/collected_charge.py
@@ -17,11 +17,12 @@ from summarize_parameters import read_summarized_data
 from lmfit import Model, Parameter, report_fit
 from uncertainties import ufloat
 from utils import my_std, save_dataframe, kMAD, get_function_arguments_names, hex_to_rgba, resample_by_n_trigger, starmap_checking_arguments_names
+from clean_beta_scan import tag_n_trigger_as_background_according_to_the_result_of_clean_beta_scan
 
 def signal_probability_density_model(x, landau_x_mpv, landau_xi, landau_gauss_sigma):
 	return langauss.pdf(x, landau_x_mpv, landau_xi, landau_gauss_sigma)
 
-def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_background:dict, time_from_trigger_signal:dict, signal_name_trigger:str, n_bootstraps:int, collected_charge_variable_name:str, force:bool=False):
+def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_background:dict, time_from_trigger_signal:dict, signal_name_trigger:str, n_bootstraps:int, collected_charge_variable_name:str, force:bool=False, use_clean_beta_scan:bool=True):
 	"""Fit a Landau (Langauss) to a variable and extract the MPV.
 	
 	Arguments
@@ -43,7 +44,6 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 		```
 		time_from_trigger = parsed_from_waveforms.loc[signal_name,'t_50 (s)'] - parsed_from_waveforms.loc[signal_name_trigger,'t_50 (s)']
 		```
-		 
 	time_from_trigger_signal: dict
 		A dictionary where the keys are names of signals present in the
 		data and the items are tuples of the form `(float,float)` specifying
@@ -65,6 +65,9 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 		Variable that best represents the collected charge in which one
 		expects the Landau distribution. Usually this is `'Amplitude (V)'`
 		or `'Collected charge (V s)'` or so.
+	use_clean_beta_scan: bool, default `True`
+		Whether to use the information from the task `clean_beta_scan`. If
+		`False`, it is ignored. If `True` then only 'clean events' are used.
 	"""
 	bureaucrat.check_these_tasks_were_run_successfully('beta_scan')
 	
@@ -85,6 +88,10 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 		_B = data.query(f'signal_name == "{signal_name_trigger}"')['t_50 (s)'].reset_index('signal_name', drop=True)
 		data['Time from trigger (s)'] = _A-_B
 		
+		if use_clean_beta_scan:
+			data = tag_n_trigger_as_background_according_to_the_result_of_clean_beta_scan(bureaucrat, data)
+			data = data.query('is_background == False')
+		
 		if signal_name_trigger not in set(data.index.get_level_values('signal_name')):
 			raise ValueError(f'`signal_name_trigger` {repr(signal_name_trigger)} not present among the signals of the dataset which are {sorted(set(data.index.get_level_values("signal_name")))}. ')
 		
@@ -94,7 +101,9 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 		# Because the scipy.curve_fit works better when the numbers are close to 1, I rescale the data. Otherwise it fails.
 		scale_factor_for_comfortable_fit = numpy.nanmean(data[variable])**-1
 		scaled_variable_name = variable + ' SCALED'
-		data[scaled_variable_name] = data[variable]*scale_factor_for_comfortable_fit
+		with warnings.catch_warnings():
+			warnings.filterwarnings("ignore")
+			data[scaled_variable_name] = data[variable]*scale_factor_for_comfortable_fit
 		
 		fig_background = go.Figure()
 		fig = go.Figure()
@@ -126,7 +135,10 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 				if len(samples_background)==0:
 					raise RuntimeError(f'No background events were found for `time_from_trigger_background[signal_name]` = {repr(time_from_trigger_background[signal_name])}. ')
 				
-				background_probability_density_model = gaussian_kde(samples_background)
+				try:
+					background_probability_density_model = gaussian_kde(samples_background)
+				except Exception:
+					background_probability_density_model = lambda x: numpy.array([0]*len(x))
 				
 				if n_bootstrap == 0: # Means with the real data so do a plot.
 					line_color = next(colors)
@@ -155,7 +167,7 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 				
 				hist, bin_edges = numpy.histogram(samples_with_signal_and_background, bins='auto', density=False)
 				bin_centers = bin_edges[:-1] + numpy.diff(bin_edges)/2
-				# Add an extra bin to the left:
+				# ~ # Add an extra bin to the left:
 				hist = numpy.insert(hist, 0, sum(samples_with_signal_and_background<bin_edges[0]))
 				bin_centers = numpy.insert(bin_centers, 0, bin_centers[0]-numpy.diff(bin_edges)[0])
 				# Add an extra bin to the right:
@@ -163,35 +175,34 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 				bin_centers = numpy.append(bin_centers, bin_centers[-1]+numpy.diff(bin_edges)[0])
 				
 				def probability_density_model(x, landau_x_mpv, landau_xi, landau_gauss_sigma, signal_to_background_fraction):
+					# This integrated to 1.
 					return signal_to_background_fraction*signal_probability_density_model(x, landau_x_mpv, landau_xi, landau_gauss_sigma) + (1-signal_to_background_fraction)*background_probability_density_model(x)
 				
 				def events_count_model(x, landau_x_mpv, landau_xi, landau_gauss_sigma, n_signal, n_background):
+					# This integrates to `(n_signal+n_background)*bin_size` with `bin_size` being the size of the first bin (assuming all identical).
 					N = n_background+n_signal
 					return probability_density_model(x, landau_x_mpv, landau_xi, landau_gauss_sigma, n_signal/N)*N*numpy.diff(bin_edges)[0]
-				
-				PARAMS_LABELS = {
-					'landau_x_mpv': 'x<sub>MPV</sub>', 
-					'landau_xi': 'ξ', 
-					'landau_gauss_sigma': 'σ', 
-					'n_signal': 'n<sub>signal</sub>', 
-					'n_background': 'n<sub>background</sub>',
-				}
 				
 				model = Model(events_count_model, independent_vars=['x'])
 				params = model.make_params()
 				params['landau_x_mpv'].value = bin_centers[2:][numpy.argmax(hist[2:])] # Remove the two first bin because usually the background is there.
 				params['landau_x_mpv'].min = 0
-				params['landau_xi'].value = median_abs_deviation(samples_with_signal_and_background)/5
-				params['landau_xi'].min = 0
+				params['landau_x_mpv'].vary = True
+				params['landau_xi'].value = median_abs_deviation(samples_with_signal_and_background)
+				params['landau_xi'].min = params['landau_xi'].value/10
+				params['landau_xi'].max = params['landau_xi'].value*10
+				params['landau_xi'].vary = True
 				params['landau_gauss_sigma'].value = median_abs_deviation(samples_with_signal_and_background)
-				params['landau_gauss_sigma'].min = 0
+				params['landau_gauss_sigma'].min = params['landau_gauss_sigma'].value/10
+				params['landau_gauss_sigma'].max = params['landau_gauss_sigma'].value*10
+				params['landau_gauss_sigma'].vary = True
 				_background_rate_estimation = len(samples_background)/abs(numpy.diff(time_from_trigger_background[signal_name])[0])
 				_sigmas = 1
 				params['n_background'].value = _background_rate_estimation*abs(numpy.diff(time_from_trigger_signal[signal_name])[0])
-				params['n_background'].min = params['n_background'].value - _sigmas*params['n_background'].value**.5
+				params['n_background'].min = max(0, params['n_background'].value - _sigmas*params['n_background'].value**.5)
 				params['n_background'].max = params['n_background'].value + _sigmas*params['n_background'].value**.5
 				params['n_signal'].value = len(samples_with_signal_and_background) - params['n_background'].value
-				params['n_signal'].min = params['n_signal'].value - _sigmas*params['n_signal'].value
+				params['n_signal'].min = max(0, params['n_signal'].value - _sigmas*params['n_signal'].value)
 				params['n_signal'].max = params['n_signal'].value + _sigmas*params['n_signal'].value
 				params['n_background'].vary = True
 				params['n_signal'].vary = True
@@ -203,6 +214,7 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 					try:
 						result = model.fit(hist, params, x=bin_centers)
 						_ = {param_name: result.params[param_name].value for param_name in result.params}
+						
 					except Exception as e:
 						warnings.warn(f'Cannot fit signal_name {repr(signal_name)}, variable {repr(variable)}, n_bootstrap {repr(n_bootstrap)}, run {repr(bureaucrat.run_name)}. Reason: {e}')
 						_ = {param_name: numpy.nan for param_name in params}
@@ -212,6 +224,13 @@ def fit_Landau_and_extract_MPV(bureaucrat:RunBureaucrat, time_from_trigger_backg
 				params_to_save.append(_)
 				
 				if n_bootstrap == 0 and 'result' in locals(): # Means with real data.
+					PARAMS_LABELS = {
+						'landau_x_mpv': 'x<sub>MPV</sub>', 
+						'landau_xi': 'ξ', 
+						'landau_gauss_sigma': 'σ', 
+						'n_signal': 'n<sub>signal</sub>', 
+						'n_background': 'n<sub>background</sub>',
+					}
 					hist_max_for_the_plot_that_does_not_handle_it_automatically_yet = max(hist_max_for_the_plot_that_does_not_handle_it_automatically_yet,hist.max())
 					fig.add_trace(
 						scatter_histogram(
@@ -343,6 +362,7 @@ def fit_Landau_and_extract_MPV_sweeping_voltage(bureaucrat:RunBureaucrat, time_f
 							n_bootstraps = n_bootstraps,
 							collected_charge_variable_name = collected_charge_variable_name,
 							force = force,
+							use_clean_beta_scan = True,
 						)
 						for subrun in subruns
 					],
@@ -424,7 +444,7 @@ def collected_charge_in_Coulomb(bureaucrat:RunBureaucrat, collected_charge_varia
 		
 		for variable in set(data.index.get_level_values('variable')):
 			fig = line(
-				title = f'Collected charge<br><sup>{bureaucrat.run_name}</sup>',
+				title = f'Collected charge using {collected_charge_variable_name}<br><sup>{bureaucrat.run_name}</sup>',
 				data_frame = data.reset_index(drop=False).query(f'variable=="{variable}"').sort_values(['Bias voltage (V) mean','signal_name']),
 				x = 'Bias voltage (V) mean',
 				y = 'Collected charge (C)',
@@ -473,28 +493,31 @@ if __name__ == '__main__':
 	
 	args = parser.parse_args()
 	
-	COLLECTED_CHARGE_VARIABLE_NAME = 'Collected charge (V s)'
-	CONVERSION_FACTOR = {
-		'Amplitude (V)': dict(value=5.7e12, error=.250e12),
-		'Collected charge (V s)': dict(value=5e3,error=.250e3),
-	}
-	
 	bureaucrat = RunBureaucrat(Path(args.directory))
 	subruns = bureaucrat.list_subruns_of_task('beta_scan_sweeping_bias_voltage')
-	SIGNALS_NAMES = {'DUT_CH1'}
-	fit_Landau_and_extract_MPV_sweeping_voltage(
-		bureaucrat = bureaucrat,
-		time_from_trigger_signal = {subrun.run_name:{_:(1e-9,2e-9) for _ in SIGNALS_NAMES} for subrun in subruns},
-		time_from_trigger_background = {subrun.run_name:{_:(-5e-9,0e-9) for _ in SIGNALS_NAMES} for subrun in subruns},
-		signal_name_trigger = 'MCP-PMT',
-		n_bootstraps = args.n_bootstraps,
-		force = args.force,
-		collected_charge_variable_name = COLLECTED_CHARGE_VARIABLE_NAME,
-		number_of_processes = max(multiprocessing.cpu_count()-1,1),
-	)
-	# ~ collected_charge_in_Coulomb(
-		# ~ bureaucrat = bureaucrat, 
-		# ~ collected_charge_variable_name = COLLECTED_CHARGE_VARIABLE_NAME, 
-		# ~ conversion_factor_to_divide_by = CONVERSION_FACTOR[COLLECTED_CHARGE_VARIABLE_NAME]['value'],
-		# ~ conversion_factor_to_divide_by_error = CONVERSION_FACTOR[COLLECTED_CHARGE_VARIABLE_NAME]['error'],
-	# ~ )
+	SIGNALS_NAMES = {'DUT'}
+	
+	CALIBRATION_FACTORS_COULOMB = {
+		'Amplitude (V)': {'val': 5.67e12, 'err': .25e12},
+		'Collected charge (V s)': {'val': 5050, 'err': 250},
+	}
+	
+	for variable in ['Amplitude (V)']:
+		with warnings.catch_warnings():
+			warnings.filterwarnings("ignore", 'Your `task_name` is ')
+			fit_Landau_and_extract_MPV_sweeping_voltage(
+				bureaucrat = bureaucrat,
+				time_from_trigger_background = {subrun.run_name:{_:(1e-9,1.5e-9) for _ in SIGNALS_NAMES} for subrun in subruns},
+				time_from_trigger_signal = {subrun.run_name:{_:(1.5e-9,1.83e-9) for _ in SIGNALS_NAMES} for subrun in subruns},
+				signal_name_trigger = 'MCP-PMT',
+				n_bootstraps = args.n_bootstraps,
+				force = args.force,
+				collected_charge_variable_name = variable,
+				number_of_processes = max(multiprocessing.cpu_count()-2,1),
+			)
+			collected_charge_in_Coulomb(
+				bureaucrat = bureaucrat,
+				collected_charge_variable_name = variable,
+				conversion_factor_to_divide_by = CALIBRATION_FACTORS_COULOMB[variable]['val'],
+				conversion_factor_to_divide_by_error = CALIBRATION_FACTORS_COULOMB[variable]['err'],
+			)
